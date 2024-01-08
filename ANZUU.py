88c1ee2fc00c2eceb433c9f3b062e55143283f02
B-Panther <150675493+B-Panther@users.noreply.github.com>
2024-01-08 22:28:14 +0500

    Update ANZUU.py

--- ANZUU.py
+++ ANZUU.py
@@ -1,5 +1,13 @@
-import os
-import time
-os.system("git pull")
-print('[•] All Error Fix Done');time.sleep (1.1)
-import ANZUU
+import os, platform, time, sys
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+ranaxd = platform.architecture()[0]
+if ranaxd == '64bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+ import rana1
+elif ranaxd == '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit which is not supported yet')
