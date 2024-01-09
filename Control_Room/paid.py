48b52799a97b71d905ec543adfb1620934d9ed0a
B-Panther <150675493+B-Panther@users.noreply.github.com>
2024-01-09 22:06:25 +0500

    Update paid.py

--- Control_Room/paid.py
+++ Control_Room/paid.py
@@ -1,10 +1,2 @@
-# Server script content
+action = 'Activate'
 
-# Define the action to be performed on the client
-# 'Free' represents the action for Free()
-# 'Activate' represents the action for Activate()
-# 'Deactivate' represents the action for Deactivate()
-action = 'Activate'  # Replace this with the desired action
-
-# Send the action to the client
-print(action)
