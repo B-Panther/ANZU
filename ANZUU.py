034961b689afda9523d336e78d2c87a37838913a
B-Panther <150675493+B-Panther@users.noreply.github.com>
2024-01-10 20:07:29 +0500

    Update ANZUU.py

--- ANZUU.py
+++ ANZUU.py
@@ -2,12 +2,11 @@ import os, platform, time, sys
 try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
- 
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 Anzu = platform.architecture()[0]
 if Anzu == '64bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
- import ANZUUUU
+ import ANZXU
 elif Anzu == '32bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit which is not supported yet')
