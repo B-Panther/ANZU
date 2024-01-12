2237050ab0a90b02f13a85675a704d4d549b251e
B-Panther <150675493+B-Panther@users.noreply.github.com>
2024-01-12 11:59:45 +0500

    Update and rename Loc to Loc.py

---
+++ Control_Room/Loc.py
@@ -0,0 +1,17 @@
+code1 = result.split(";FBLC/")[1].split(";")[0]
+loc1 = selected_locale.split("_")[1]
+
+code2 = result.split(";FBLC/")[1].split(";")[0]
+loc2 = selected_locale.split("_")[1]
+
+code3 = result.split(";FBLC/")[1].split(";")[0]
+loc3 = selected_locale.split("_")[1]
+
+code4 = result.split(";FBLC/")[1].split(";")[0]
+loc4 = selected_locale.split("_")[1]
+
+code5 = result.split(";FBLC/")[1].split(";")[0]
+loc5 = selected_locale.split("_")[1]
+
+code6 = result.split(";FBLC/")[1].split(";")[0]
+loc6 = selected_locale.split("_")[1]
